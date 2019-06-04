0a4c3014f0488636550e91187a9d9b6b347bd44d
Ryan <ryanburdick14@gmail.com>
2019-06-03 23:29:48 -0500

    all charts animate

--- static/js/app.js
+++ static/js/app.js
@@ -38,197 +38,196 @@ var Y_to_pred
 // is the date
 var Date1
 
-function returnData(e){
+function returnData(e) {
 
-  fetch("/predict/"+modelType+"/"+modelMonth+"/"+modelDay+"/"+modelYear, function(z){})
+  fetch("/predict/" + modelType + "/" + modelMonth + "/" + modelDay + "/" + modelYear, function (z) {})
     .then(response => response.json())
     .then(data => {
-    Y_prev = data.Y_prev;
-    Y_prob_a = data.Y_prob[0].toFixed(3);
-    Y_prob_b = data.Y_prob[1].toFixed(3);
-    Y_prob_c = data.Y_prob[2].toFixed(3);
-    Y_to_pred = data.Y_to_pred;
-    Date1 = data.date;
-    console.log("Selected date: " + Date1);
-    console.log("The DFF of the selected date: " + Y_to_pred);
-    console.log("The previous day's DFF Value: " + Y_prev);
-    console.log("Our predicted probability the DFF will go up: " + Y_prob_a);
-    console.log("Our predicted probability the DFF will stay the same: " + Y_prob_b);
-    console.log("Our predicted probability the DFF will go down: " + Y_prob_c);
+      Y_prev = data.Y_prev;
+      Y_prob_a = data.Y_prob[0].toFixed(3);
+      Y_prob_b = data.Y_prob[1].toFixed(3);
+      Y_prob_c = data.Y_prob[2].toFixed(3);
+      Y_to_pred = data.Y_to_pred;
+      Date1 = data.date;
+      console.log("Selected date: " + Date1);
+      console.log("The DFF of the selected date: " + Y_to_pred);
+      console.log("The previous day's DFF Value: " + Y_prev);
+      console.log("Our predicted probability the DFF will go up: " + Y_prob_a);
+      console.log("Our predicted probability the DFF will stay the same: " + Y_prob_b);
+      console.log("Our predicted probability the DFF will go down: " + Y_prob_c);
 
-    // chart 1
-    var Date2 = new Date(Date1);
-    Date2.setDate(Date2.getDate() - 1);
-    Date2 = (Date2.getMonth() + 1) + '/' + Date2.getDate() + '/' +  Date2.getFullYear();
-    console.log("Selected date: " + Date2);
+      // chart 1
+      var Date2 = new Date(Date1);
+      Date2.setDate(Date2.getDate() - 1);
+      Date2 = (Date2.getMonth() + 1) + '/' + Date2.getDate() + '/' + Date2.getFullYear();
+      console.log("Selected date: " + Date2);
 
-    var
+      var
         Y_Axis_DFF_Data = [Y_prev, Y_to_pred],
         X_Axis_Date_Data = [Date2, Date1];
 
-    // Chart 2
-    var
+      // Chart 2
+      var
         Y_Axis_DFF_Data2 = [Y_prob_a, Y_prob_b, Y_prob_c],
         X_Axis_Date_Data2 = ["go up", "stay the same", "go down"];
 
 
+      function makeResponsive() {
+        // if the SVG area isn't empty when the send button is clicked,
+        // remove it
+        var svgArea = d3.select("#chart").select("svg");
 
-    function makeResponsive() {
-
-      // if the SVG area isn't empty when the send button is clicked,
-      // remove it
-      var svgArea = d3.select("#chart").select("svg");
-
-      if (!svgArea.empty()) {
+        if (!svgArea.empty()) {
           svgArea.remove();
-      }
+        }
 
-      // svg params
-      var svgHeight = 275;
-      var svgWidth = 350;
+        // svg params
+        var svgHeight = 275;
+        var svgWidth = 350;
 
-      // margins
-      var margin = {
+        // margins
+        var margin = {
           top: 50,
           right: 50,
           bottom: 50,
           left: 50
-      };
+        };
 
-      // chart area minus margins
-      var chartHeight = svgHeight - margin.top - margin.bottom;
-      var chartWidth = svgWidth - margin.left - margin.right;
+        // chart area minus margins
+        var chartHeight = svgHeight - margin.top - margin.bottom;
+        var chartWidth = svgWidth - margin.left - margin.right;
 
-      // create svg container
-      var svg = d3.select("#chart").append("svg")
+        // create svg container
+        var svg = d3.select("#chart").append("svg")
           .attr("height", svgHeight)
           .attr("width", svgWidth);
 
-      // shift everything over by the margins
-      var chartGroup = svg.append("g")
+        // shift everything over by the margins
+        var chartGroup = svg.append("g")
           .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-      var y_domain_max = (d3.max(Y_Axis_DFF_Data)*1.50).toFixed(3);
-      var y_domain_min = (d3.max(Y_Axis_DFF_Data)/1.50).toFixed(3);
+        var y_domain_max = (d3.max(Y_Axis_DFF_Data)*1.5).toFixed(3);
+        var y_domain_min = (d3.max(Y_Axis_DFF_Data)/7).toFixed(3);
 
-      // scale y to chart height
-      var yScale = d3.scaleLinear()
+        // scale y to chart height
+        var yScale = d3.scaleLinear()
           .domain([y_domain_min, y_domain_max])
           .range([chartHeight, 0]);
 
-      // scale x to chart width
-      var xScale = d3.scaleBand()
+        // scale x to chart width
+        var xScale = d3.scaleBand()
           .domain(X_Axis_Date_Data)
           .range([0, chartWidth])
           .padding(0.1);
 
-      // create axes
-      var yAxis = d3.axisLeft(yScale);
-      var xAxis = d3.axisBottom(xScale);
+        // create axes
+        var yAxis = d3.axisLeft(yScale);
+        var xAxis = d3.axisBottom(xScale);
 
-      // set x to the bottom of the chart
-      chartGroup.append("g")
+        // set x to the bottom of the chart
+        chartGroup.append("g")
           .attr("transform", `translate(0, ${chartHeight})`)
           .call(xAxis);
 
-      // set y to the y axis
-      chartGroup.append("g")
+        // set y to the y axis
+        chartGroup.append("g")
           .call(yAxis);
 
-      chartGroup.selectAll("circle")
+        chartGroup.selectAll("circle")
           .data(Y_Axis_DFF_Data)
           .enter()
           .append("circle")
+          .transition()
+          .delay(function (d, i) {
+            return (i * 3)
+          })
+          .duration(6000)
           .attr("cx", (d, i) => xScale(X_Axis_Date_Data[i]) + 55)
           .attr("cy", d => yScale(d))
           .attr("r", 6)
           // .attr("height", d => chartHeight - yScale(d))
           .attr("fill", "teal")
-          // event listener for onclick event
-          .on("click", function(d, i) {
-            alert(`DFF: ${Y_Axis_DFF_Data}`);
-          });
 
         // Add the text label for the Y axis
-    chartGroup.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left)
-        .attr("x",0 - (chartHeight / 2))
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        .text("Fed Rate (%)");
+        chartGroup.append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 0 - margin.left)
+          .attr("x", 0 - (chartHeight / 2))
+          .attr("dy", "1em")
+          .style("text-anchor", "middle")
+          .text("Fed Rate (%)");
 
         // Add the text label for the x axis
-    chartGroup.append("text")
-        .attr("transform", "translate(" + (chartWidth / 2) + " ," + ((chartHeight - 5) + margin.bottom) + ")")
-        .style("text-anchor", "middle")
-        .text("Date");
+        chartGroup.append("text")
+          .attr("transform", "translate(" + (chartWidth / 2) + " ," + ((chartHeight - 5) + margin.bottom) + ")")
+          .style("text-anchor", "middle")
+          .text("Date");
 
-      // Chart 2
-      // if the SVG2 area isn't empty...
-      var svgArea2 = d3.select("#chart2").select("svg");
+        // Chart 2
+        // if the SVG2 area isn't empty...
+        var svgArea2 = d3.select("#chart2").select("svg");
 
-      if (!svgArea2.empty()) {
+        if (!svgArea2.empty()) {
           svgArea2.remove();
-      }
+        }
 
-      // svg2 params
-      var svgHeight2 = 275;
-      var svgWidth2 = 350;
+        // svg2 params
+        var svgHeight2 = 275;
+        var svgWidth2 = 350;
 
-      // margins
-      var margin2 = {
+        // margins
+        var margin2 = {
           top: 50,
           right: 50,
           bottom: 50,
           left: 50
-      };
+        };
 
-      // chart area minus margins
-      var chartHeight2 = svgHeight2 - margin2.top - margin2.bottom;
-      var chartWidth2 = svgWidth2 - margin2.left - margin2.right;
+        // chart area minus margins
+        var chartHeight2 = svgHeight2 - margin2.top - margin2.bottom;
+        var chartWidth2 = svgWidth2 - margin2.left - margin2.right;
 
-      // create svg2 container
-      var svg2 = d3.select("#chart2").append("svg")
+        // create svg2 container
+        var svg2 = d3.select("#chart2").append("svg")
           .attr("height", svgHeight2)
           .attr("width", svgWidth2);
 
-      // shift everything over by the margins
-      var chartGroup2 = svg2.append("g")
+        // shift everything over by the margins
+        var chartGroup2 = svg2.append("g")
           .attr("transform", `translate(${margin2.left}, ${margin2.top})`);
 
-      // scale y to chart height
-      var yScale2 = d3.scaleLinear()
+        // scale y to chart height
+        var yScale2 = d3.scaleLinear()
           .domain([0, d3.max(Y_Axis_DFF_Data2)])
           .range([chartHeight2, 0]);
 
-      // scale x to chart width
-      var xScale2 = d3.scaleBand()
+        // scale x to chart width
+        var xScale2 = d3.scaleBand()
           .domain(X_Axis_Date_Data2)
           .range([0, chartWidth2])
           .padding(0.1);
 
-      // create axes2
-      var yAxis2 = d3.axisLeft(yScale2);
-      var xAxis2 = d3.axisBottom(xScale2);
+        // create axes2
+        var yAxis2 = d3.axisLeft(yScale2);
+        var xAxis2 = d3.axisBottom(xScale2);
 
-      // set x to the bottom of the chart
-      chartGroup2.append("g")
+        // set x to the bottom of the chart
+        chartGroup2.append("g")
           .attr("transform", `translate(0, ${chartHeight2})`)
           .call(xAxis2);
-          // .text("probability")
+        // .text("probability")
 
-      // set y to the y axis
-      chartGroup2.append("g")
+        // set y to the y axis
+        chartGroup2.append("g")
           .call(yAxis2);
 
-      chartGroup2.selectAll("rect")
+        chartGroup2.selectAll("rect")
           .data(Y_Axis_DFF_Data2)
           .enter()
           .append("rect")
           .attr("x", (d, i) => xScale2(X_Axis_Date_Data2[i]))
           .attr("y", chartHeight2)
-          .transition().duration(5000)
+          .transition().duration(2000)
           .ease(d3.easeExp)
           .attr("y", d => yScale2(d))
           .attr("width", xScale2.bandwidth())
@@ -236,28 +235,27 @@ function returnData(e){
           // .attr("height", d => chartHeight - yScale2(d))
           .attr("fill", "teal")
           // event listener for onclick event
-          .on("click", function(d, i) {
+          .on("click", function (d, i) {
             alert(`DFF: ${Y_Axis_DFF_Data2}`);
           })
 
-          // Add the text label for the Y axis
-      chartGroup2.append("text")
+        // Add the text label for the Y axis
+        chartGroup2.append("text")
           .attr("transform", "rotate(-90)")
           .attr("y", 0 - margin2.left)
-          .attr("x",0 - (chartHeight2 / 2))
+          .attr("x", 0 - (chartHeight2 / 2))
           .attr("dy", "1em")
           .style("text-anchor", "middle")
           .text("Probability (%)");
 
-          // Add the text label for the x axis
-      chartGroup2.append("text")
+        // Add the text label for the x axis
+        chartGroup2.append("text")
           .attr("transform", "translate(" + (chartWidth2 / 2) + " ," + ((chartHeight2 - 5) + margin2.bottom) + ")")
           .style("text-anchor", "middle")
           .text("Model Prediction");
-
-    }
-    makeResponsive();
-      })
+      }
+      makeResponsive();
+    })
 }
 
 (async function () {
@@ -421,12 +419,12 @@ function returnData(e){
   chartGroup.append("text")
     .attr("transform", "rotate(-90)")
     .attr("y", 0 - margin.left)
-    .attr("x",0 - (chartHeight / 2))
+    .attr("x", 0 - (chartHeight / 2))
     .attr("dy", "1em")
     .style("text-anchor", "middle")
     .text("Fed Rate (%)");
 
-    // Add the text label for the x axis
+  // Add the text label for the x axis
   chartGroup.append("text")
     .attr("transform", "translate(" + (chartWidth / 2) + " ," + ((chartHeight - 5) + margin.bottom) + ")")
     .style("text-anchor", "middle")
